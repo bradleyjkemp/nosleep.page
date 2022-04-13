a6389566bf614e40b21188332944258f4403b332
Bradley Kemp <bradleyjkemp@gmx.co.uk>
2022-04-13 12:04:34 +0100

    Attempt to enable wakelock on page load

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,30 @@
-import NoSleep from 'nosleep.js';
+import NoSleep from 'nosleep.js'
 
-var noSleep = new NoSleep();
+var noSleep = new NoSleep()
+
+function updateSwitchStatus() {
+    if (noSleep.isEnabled) {
+        console.log("noSleep is enabled")
+        document.getElementById("preventSleepSwitch").checked = true
+        document.getElementById("hero").classList.add("is-success")
+        document.getElementById('favicon').setAttribute('href', 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☕️</text></svg>')
+    } else {
+        console.log("noSleep is disabled")
+        document.getElementById("preventSleepSwitch").checked = false
+        document.getElementById("hero").classList.remove("is-success")
+        document.getElementById('favicon').setAttribute('href', 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😴</text></svg>')
+    }
+}
 
 document.getElementById("preventSleepSwitch").addEventListener("click", (evt) => {
     const preventSleep = evt.target.checked
 
     if (preventSleep) {
-        noSleep.enable()
-        document.getElementById('favicon').setAttribute('href','data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☕️</text></svg>')
+        noSleep.enable().then(updateSwitchStatus)
     } else {
         noSleep.disable()
-        document.getElementById('favicon').setAttribute('href','data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😴</text></svg>')
+        updateSwitchStatus()
     }
 })
+
+noSleep.enable().then(updateSwitchStatus)
